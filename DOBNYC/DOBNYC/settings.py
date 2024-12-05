c51e54ebad395fed958768837e0f39a934a3d6c0
Seab0t <136824322+Seab0t@users.noreply.github.com>
2024-12-05 16:26:43 -0500

    Update settings.py

--- DOBNYC/DOBNYC/settings.py
+++ DOBNYC/DOBNYC/settings.py
@@ -36,7 +36,7 @@ CONCURRENT_REQUESTS = 32
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.3
+DOWNLOAD_DELAY = 0.5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -98,7 +98,7 @@ AUTOTHROTTLE_START_DELAY = 0.7
 AUTOTHROTTLE_MAX_DELAY = 40
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 20.0
+AUTOTHROTTLE_TARGET_CONCURRENCY = 10.0
 # Enable showing throttling stats for every response received:
 #AUTOTHROTTLE_DEBUG = False
 
